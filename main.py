340d22e27dfeafc2df5c64673e12769ec53811c6
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-03 12:44:30 -0300

    edit: update
    Style and comments update

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #pgzero
 
 """
-Version actual: [M7.L1: Actividad 1/9]
+Version actual: [M7.L1: Actividad #1]
+
+Kodland: https://kenney.nl/assets/roguelike-caves-dungeons
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
 """
 
 # Ventana de juego hecha de celdas
-celda = Actor('border')
+celda = Actor('border') # Celda que voy a utilizar como referencia para mi mapa
+
 size_w = 5 # Ancho del mapa en celdas
 size_h = 5 # Altura del mapa en celdas
 
-WIDTH =  celda.width  * size_w
-HEIGHT = celda.height * size_h
+WIDTH =  celda.width  * size_w # Ancho de la ventana (en píxeles)
+HEIGHT = celda.height * size_h #  Alto de la ventana (en píxeles)
 
-TITLE = "Mazmorras" # Título de la ventana de juego
+TITLE = "Rogue-like: Mazmorras" # Título de la ventana de juego
 FPS = 30 # Número de fotogramas por segundo
