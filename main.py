ac117f308e636dcf1ea71c512a7b94920204ac4b
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-03 13:04:32 -0300

    [M7.L1: Actividad #2]
    Ampliamos tamaño mapa (en casillas) y agregamos lista que representa el mapa a dibujar

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #pgzero
 
 """
-Version actual: [M7.L1: Actividad #1]
+Version actual: [M7.L1: Actividad #2]
+
+Objetivo: Ampliamos tamaño mapa (en casillas)
+          y agregamos lista que representa el mapa a dibujar
 
 Kodland: https://kenney.nl/assets/roguelike-caves-dungeons
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
@@ -10,11 +13,19 @@ packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
 # Ventana de juego hecha de celdas
 celda = Actor('border') # Celda que voy a utilizar como referencia para mi mapa
 
-size_w = 5 # Ancho del mapa en celdas
-size_h = 5 # Altura del mapa en celdas
+size_w = 7 # Ancho del mapa en celdas
+size_h = 7 # Altura del mapa en celdas
 
 WIDTH =  celda.width  * size_w # Ancho de la ventana (en píxeles)
 HEIGHT = celda.height * size_h #  Alto de la ventana (en píxeles)
 
 TITLE = "Rogue-like: Mazmorras" # Título de la ventana de juego
 FPS = 30 # Número de fotogramas por segundo
+
+mapa = [ [0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 2, 1, 3, 1, 0],
+         [0, 1, 1, 2, 1, 1, 0],
+         [0, 3, 2, 1, 1, 3, 0],
+         [0, 1, 1, 1, 3, 1, 0],
+         [0, 1, 3, 1, 1, 2, 0],
+         [0, 0, 0, 0, 0, 0, 0] ]
